8c7c0b56f656a4cc174ca2bea1e82cc2e09c5bfe
Adam Howard <howarda8@nku.edu>
2014-04-04 02:06:40 +0000

    Add additional logic and fields for list views

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -16,6 +16,28 @@ class ListsController < ApplicationController
     @list = List.find(params[:id])
   end
   
+  def edit
+    @list = List.find(params[:id])
+  end
+  
+  def update
+    @list = List.find(params[:id])
+    
+    if @list.update(params[:list].permit(:title, :description))
+      flash[:success] = "You have successfully edited " + @list.title
+      redirect_to lists_path      
+    else
+      render 'edit'
+    end
+  end
+  
+  def destroy
+    @list = List.find(params[:id])
+    @list.destroy
+    
+    redirect_to lists_path
+  end
+  
   private
   def list_params
     params.require(:list).permit(:title, :description)
