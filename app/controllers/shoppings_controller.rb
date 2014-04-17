a778b086395359ffa5472d4e90db54bee51f43c2
Adam Howard <howarda8@nku.edu>
2014-04-17 03:08:30 +0000

    Add logic to create shopping lists
    Logic now takes into account the threshold specified
    by the user.

--- app/controllers/shoppings_controller.rb
+++ app/controllers/shoppings_controller.rb
@@ -1,2 +1,17 @@
 class ShoppingsController < ApplicationController
+  def create
+    @list = List.find_by_title(params[:list][:list_id])
+    @items = @list.items.to_a
+    @low_items = []
+    
+    @items.each do |item|
+      if(item.quantity != nil)
+        if(item.quantity <= params[:threshold].to_i)
+        @low_items.push(item)
+        end
+      else
+        @low_items.push(item)
+      end
+    end
+  end
 end
