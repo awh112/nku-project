1f800b58263c12deafe544c43fb7c418110e369c
Adam Howard <howarda8@nku.edu>
2014-04-18 02:00:13 +0000

    Complete functionality to edit list items.
    Add functionality to allow for users to edit the items in their
    lists.  Also update the readme file to reflect the new user
    stories.

--- README.md
+++ README.md
@@ -16,3 +16,20 @@
   I want to create items
   So that I can know how many of each item I have and add those items to a list
   ```
+  
+##Week 2 - 4/18/14
+
+* Add the functionality to allow for the generation of shopping lists based on data in their lists.
+  * A user can click 'Generate Shopping List' to generate a list based on a selected threshold.
+  ```
+  As a user
+  I want to generate a shopping list
+  So that I can know what items I am low on
+  ```
+  
+  * A user can edit the quantity of items in lists
+  ```
+  As a user
+  I want to edit my list items
+  So that I can update how many of them I have
+  ```
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,4 +4,22 @@ class ItemsController < ApplicationController
     @item = @list.items.create(params[:item].permit(:name, :description, :quantity))
     redirect_to list_path(@list)
   end
+  
+  def edit
+    @list = List.find(params[:list_id])
+    @item = @list.items.find(params[:id])
+  end
+  
+  def update
+    @list = List.find(params[:list_id])
+    @item = @list.items.find(params[:id])
+    
+    @item.name = params[:item][:name]
+    @item.description = params[:item][:description]
+    @item.quantity = params[:item][:quantity]
+    @item.save    
+    
+    flash[:success] = "You have successfully edited " + @item.name
+    redirect_to list_path(@list)
+  end
 end
